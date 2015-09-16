aa72e58c40ea895057b58510ee5cedd7730a5c96
Kendrick Choate <kendrickchoate@gmail.com>
2015-09-15 19:55:54 -0500

    Initial game commit

---
+++ Assets/_Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float MoveHorizontal = Input.GetAxis ("Horizontal");
+		float MoveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (MoveHorizontal, 0, MoveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
